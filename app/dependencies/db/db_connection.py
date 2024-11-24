bcbbd81d41e398e5ef24ad022c62f3f4a8b70fb2
ShalevDavid1 <shalevd2014@gmail.com>
2024-11-24 19:52:33 +0200

    added DB types

---
+++ app/dependencies/db/db_connection.py
@@ -0,0 +1,27 @@
+import os
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import Session as SQLAlchemySession
+
+
+# Singleton pattern for database connection and session
+class DatabaseConnection:
+    _engine = None
+    _SessionLocal = None
+
+    @classmethod
+    def get_engine(cls):
+        if cls._engine is None:
+            DATABASE_URL = os.getenv("DATABASE_URL")  # Get the database URL from environment variable
+            if DATABASE_URL is None:
+                raise ConnectionError("DATABASE_URL environment variable does not exists")
+            # Create the engine for the first time if it does not exist
+            cls._engine = create_engine(DATABASE_URL, echo=True)
+        return cls._engine
+
+    @classmethod
+    def get_session(cls) -> SQLAlchemySession:
+        if cls._SessionLocal is None:
+            # Create session factory the first time
+            cls._SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=cls.get_engine())
+        return cls._SessionLocal()
